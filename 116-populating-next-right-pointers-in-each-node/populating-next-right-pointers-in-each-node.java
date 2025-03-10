9e567f605bbf42e74b6336e8afc92b59c0c5d715
Dani7867 <119788393+Dani7867@users.noreply.github.com>
2025-03-10 20:28:24 +0530

    Time: 1 ms (60.65%) | Memory: 45 MB (5.37%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -22,23 +22,33 @@ class Node {
 */
 
 class Solution {
-    public Node connect(Node root){
-        if(root == null){
+    public Node connect(Node root) {
+        if (root == null) {
             return root;
         }
+
         Node leftMost = root;
-        while(leftMost.left!=null){
+
+        while (leftMost != null) {
             Node current = leftMost;
-            while(current != null){
-                current.left.next = current.right;
-                if(current.next!=null){
-                    current.right.next = current.next.left;
+            Node temp = new Node(0); 
+            Node dummy = temp;
+
+            while (current != null) {
+                if (current.left != null) {
+                    temp.next = current.left;
+                    temp = temp.next;
+                }
+                if (current.right != null) {
+                    temp.next = current.right;
+                    temp = temp.next;
                 }
                 current = current.next;
             }
-            leftMost = leftMost.left;
+
+            leftMost = dummy.next; 
         }
+
         return root;
     }
-
 }
