dac184403381be881ff2a2f8f6f677351bffd30a
Dani7867 <119788393+Dani7867@users.noreply.github.com>
2025-05-31 14:03:56 +0530

    Time: 13 ms (70.99%) | Memory: 46 MB (30.69%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,19 +1,29 @@
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(char c : s.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)+1);
-        }
-        ArrayList<Map.Entry<Character,Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort((a,b)->(b.getValue()-a.getValue()));
-        StringBuilder sb = new StringBuilder();
-        for(Map.Entry<Character,Integer> c: list){
-                Character a = c.getKey();
-                Integer b = c.getValue();
-                while(b>0){
-                sb.append(a);
-                b--;}
-        }
-        return sb.toString();
+    Map<Character, Integer> freqMap = new HashMap<>();
+    for (char c : s.toCharArray()) {
+        freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
     }
+
+    List<Character>[] buckets = new List[s.length() + 1];
+    for (Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
+        int freq = entry.getValue();
+        if (buckets[freq] == null) {
+            buckets[freq] = new ArrayList<>();
+        }
+        buckets[freq].add(entry.getKey());
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = buckets.length - 1; i >= 0; i--) {
+        if (buckets[i] != null) {
+            for (char c : buckets[i]) {
+                sb.append(String.valueOf(c).repeat(i)); 
+            }
+        }
+    }
+
+    return sb.toString();
+}
+
 }
