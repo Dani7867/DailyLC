d07fad4f519afaa57b3889ad5b7c02750b85bcb1
Dani7867 <119788393+Dani7867@users.noreply.github.com>
2025-06-01 12:57:15 +0530

    Time: 14 ms (25.86%) | Memory: 50.2 MB (31.38%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,38 +1,16 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-            List<Integer> list = new ArrayList<>();
-            int c1 =0 ,c2=0;
-            int count1 = 0 , count2 = 0;
-
-            for(int x : nums){
-                if(x==c1){
-                    count1++;
-                }else if(x==c2){
-                    count2++;
-                }else if(count1 == 0){
-                    c1 = x;
-                    count1 = 1;
-                }else if(count2 == 0){
-                    c2 = x;
-                    count2 = 1;
-                }else{
-                    count1--;
-                    count2--;
-                }
+        int l = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        for(int n : nums){
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
+        for(int v : map.keySet()){
+            if(map.get(v) > l/3){
+                list.add(v);
             }
-
-            count1 = count2 = 0;
-            for(int n :nums){
-                if(n==c1) count1++;
-                else if(n==c2) count2++;
-            }
-
-            if(count1 > nums.length / 3) list.add(c1);
-            if(count2 > nums.length / 3) list.add(c2);
-            return list;
-
+        }
+        return list;
     }
-
 }
-
-    
