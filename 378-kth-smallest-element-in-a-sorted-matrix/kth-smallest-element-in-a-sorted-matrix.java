07c26c9d05fc47a77ae6b559ea0c3ba45eeb341a
Dani7867 <119788393+Dani7867@users.noreply.github.com>
2025-06-02 18:31:30 +0530

    Time: 18 ms (35.56%) | Memory: 48 MB (83.21%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,12 +1,17 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-       List<Integer> l = new ArrayList<>();
-for (int[] row : matrix) {
-    for (int num : row) {
-        l.add(num);  
-    }
-}
-    Collections.sort(l);
-return l.get(k - 1);
+        int n = matrix.length;
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a,b)->a[0]-b[0]);
+        for(int i = 0 ; i < Math.min(n,k) ; i++){
+            heap.offer(new int[]{matrix[i][0],i,0});
+        }
+        for(int i = 0 ; i<k-1 ; i++){
+            int[] smallest = heap.poll();
+            int row = smallest[1] , col = smallest[2];
+            if(col + 1 < n){
+                 heap.offer(new int[]{matrix[row][col+1],row,col+1});
+            }
+        }
+        return heap.poll()[0];
     }
 }
