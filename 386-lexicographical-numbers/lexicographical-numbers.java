57e071c0cfe5d4f276003b1c85b9564df4fb25ab
Dani7867 <119788393+Dani7867@users.noreply.github.com>
2025-06-09 17:31:42 +0530

    Time: 1 ms (100.00%) | Memory: 48.5 MB (47.19%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -1,23 +1,22 @@
-import java.util.*;
-
 class Solution {
+    private List<Integer> order;
     public List<Integer> lexicalOrder(int n) {
-        List<Integer> result = new ArrayList<>();
-        int count = 1;
-
-        for (int i = 0; i < n; i++) {
-            result.add(count);
-            if (count * 10 <= n) {
-                count *= 10;  
-            } else {
-                if (count >= n) count /= 10; 
-                count++;
-                while (count % 10 == 0) {
-                    count /= 10;  
-                }
-            }
-        }
-
-        return result;
+		order = new ArrayList<>();
+		getLexographicalOrder(n, 1);
+        return order;
     }
+
+	private void getLexographicalOrder(int n, int base) {
+		if (base > n) {
+			return;
+		}
+		order.add(base);
+		getLexographicalOrder(n, base * 10);
+		getLexographicalOrder(n, base * 10 + 1);
+		if (base % 10 == 1) {
+			for (int I = 1; I < 9; I++) {
+				getLexographicalOrder(n, base + I);
+			}
+		}
+	}
 }
