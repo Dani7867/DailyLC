9192c7b9204161f9310f1ae09372ffecf27d5b14
Danish Sheikh <119788393+Dani7867@users.noreply.github.com>
2025-06-19 14:02:37 +0530

    Time: 40 ms (73.79%) | Memory: 67.6 MB (86.30%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,30 +1,27 @@
 class Solution {
     public int minReorder(int n, int[][] connections) {
-        List<List<int[]>> g = new ArrayList<>();
-        for(int i = 0 ; i < n ; i++){
-            g.add(new ArrayList<>());
-        }
-        //direction: 1 = original, 0 = reversed
-        for(int[] x : connections){
-            int u = x[0] , v = x[1];
-            g.get(u).add(new int[]{v, 1}); // original direction u -> v
-            g.get(v).add(new int[]{u, 0}); // reverse edge added to allow DFS traversal
+        List<List<int[]>> graph = new ArrayList<>();
+        for(int i = 0 ; i<n ; i++){
+            graph.add(new ArrayList<>());
+        } 
+        for(int[] ni : connections){
+            int u = ni[0] , v = ni[1];
+            graph.get(u).add(new int[]{v,1});
+            graph.get(v).add(new int[]{u,0});
         }
         boolean[] v = new boolean[n];
-        return dfs(0,g,v);
+        return dfs(0,graph,v);
     }
 
-    int dfs(int s , List<List<int[]>> g , boolean[] v){
-        int changes = 0;
-
-        v[s] = true;
-        for(int[] n : g.get(s)){
-            int next = n[0] , needCng = n[1];
-            if(!v[next]){
-                v[next] = true;
-                changes += needCng + dfs(next,g,v);
+    int dfs(int n ,List<List<int[]>> graph,boolean[] v){
+        int c = 0;
+        v[n] = true;
+        for(int[] ne : graph.get(n)){
+            int e = ne[0] ,  needC = ne[1];
+            if(!v[e]){
+                c += needC + dfs(e,graph,v);
             }
         }
-        return changes;
+        return c;
     }
 }
