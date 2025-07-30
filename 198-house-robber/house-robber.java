693bb28132fa2b30270d4009cb53f5a91d5687c3
Danish Sheikh <119788393+Dani7867@users.noreply.github.com>
2025-07-30 21:40:47 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (76.81%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,17 +1,17 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] in = new int[nums.length];
-        int[] ex = new int[nums.length];
+        int in = nums[0];
+        int ex = 0;
 
-        ex[0] = 0;
-        in[0] = nums[0];
+        for(int i = 1 ; i<nums.length;i++){
+            int nin = ex + nums[i];
+            int nex = Math.max(ex,in);
 
-        for(int i = 1 ; i < in.length ; i++){
-            in[i] = ex[i-1] + nums[i];
-            ex[i] = Math.max(in[i-1],ex[i-1]);
+            in = nin;
+            ex = nex;
         }
 
-        return Math.max(in[nums.length-1] , ex[nums.length - 1]);
+        return Math.max(in,ex);       
     }
 
     // tle
@@ -23,4 +23,17 @@ class Solution {
 
     //     return Math.max(in,ex);
     // }
+
+    //  int[] in = new int[nums.length];
+    //     int[] ex = new int[nums.length];
+
+    //     ex[0] = 0;
+    //     in[0] = nums[0];
+
+    //     for(int i = 1 ; i < in.length ; i++){
+    //         in[i] = ex[i-1] + nums[i];
+    //         ex[i] = Math.max(in[i-1],ex[i-1]);
+    //     }
+
+    //     return Math.max(in[nums.length-1] , ex[nums.length - 1]);
 }
