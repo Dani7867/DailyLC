5e459a2456a93b69293ab0dd452d31830ee27323
Danish Sheikh <119788393+Dani7867@users.noreply.github.com>
2025-08-05 21:49:10 +0530

    Time: 2 ms (61.24%) | Memory: 44.5 MB (47.92%) - LeetSync

---
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        boolean[] used = new boolean[n]; // To mark used baskets
+        int unplaced = 0;
+
+        for (int i = 0; i < n; i++) {
+            boolean placed = false;
+            for (int j = 0; j < n; j++) {
+                if (!used[j] && baskets[j] >= fruits[i]) { 
+                    used[j] = true; // Mark basket as used
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) {
+                unplaced++;
+            }
+        }
+        return unplaced;
+    }
+}
