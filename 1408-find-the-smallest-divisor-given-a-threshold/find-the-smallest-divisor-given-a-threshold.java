fa8f84338404d19c565b7b7eaffcbb5884cf177e
Danish Sheikh <119788393+Dani7867@users.noreply.github.com>
2025-08-09 16:42:31 +0530

    Time: 30 ms (34.01%) | Memory: 48 MB (90.90%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -1,32 +1,26 @@
 class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
-        int max = Integer.MIN_VALUE;
-        for (int p : nums) {
-            max = Math.max(max, p);
+        int m = Integer.MIN_VALUE;
+        for(int n : nums){
+            m = Math.max(m,n);
         }
-        
-        int l = 1, hi = max;
-        int minS = max; // worst case
-        
-        while (l <= hi) {
-            int s = l + (hi - l) / 2;
-            if (canFinish(nums, threshold , s)) {
-                minS = s;
-                hi = s - 1;
-            } else {
-                l = s + 1;
+
+        int lo  = 1 , hi = m;
+        int ans = Integer.MAX_VALUE;
+        while(lo<=hi){
+            int mid = lo + (hi-lo) / 2;
+            long t = 0L;
+            for(int p : nums){
+                t += (int)Math.ceil(p*1.0/mid);
+            }
+            if(t <= threshold){
+                ans = mid;
+                hi = mid - 1;
+            }else{
+                lo = mid + 1;
             }
         }
-        return minS;
-    }
 
-    private boolean canFinish(int[] piles, int h , int s) {
-        long hours = 0L;                       // must be long to avoid overflow
-        for (int p : piles) {
-            hours += (p + s - 1) / s;         // integer ceiling
-            if (hours > h) return false;     // early exit
-        }
-        return hours <= h;
+        return ans;
     }
 }
-
