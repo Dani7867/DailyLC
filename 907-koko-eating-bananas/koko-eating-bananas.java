7f79e9aafb4a4e24579f4b573305beb9fc16c186
Danish Sheikh <119788393+Dani7867@users.noreply.github.com>
2025-08-10 13:56:04 +0530

    Time: 27 ms (17.38%) | Memory: 45.5 MB (24.22%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,28 +1,26 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int max = Integer.MIN_VALUE;
+        int max = 0;
         for(int p : piles){
             max = Math.max(max,p);
         }
-        int l = 1 , hi = max;
-        int minS = Integer.MAX_VALUE;
-        while(l<=hi){
-            int s = l + (hi-l) / 2;
-            if(possible(piles,s,h)){
-                minS = s;
-                hi = s - 1;
+        int lo = 1 , hi = max;
+        while(lo<=hi){
+            int speed = lo + (hi-lo) / 2;
+            //if(canEat(piles,h,speed))
+            //hi = mid-1;
+            //lo = mid+1;
+
+            long time = 0;
+            for(int b : piles){
+                time += Math.ceil((b*1.0)/speed);
+            }
+            if(time <= h){
+                hi = speed-1;
             }else{
-                l = s + 1;
+                lo = speed+1;
             }
         }
-        return minS;
-    }
-
-    private boolean possible(int[] piles , int s , int h){
-        long time = 0L;
-        for(int i = 0 ; i <  piles.length ; i++){
-             time += (piles[i] + s - 1) / s;
-        }
-        return time <= h;
+        return lo;
     }
 }
