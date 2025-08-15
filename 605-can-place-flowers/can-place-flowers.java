07c492637697257bb6d8d33906ab50b6fd90d3a7
Danish Sheikh <119788393+Dani7867@users.noreply.github.com>
2025-08-15 12:40:39 +0530

    Time: 1 ms (97.39%) | Memory: 45.9 MB (33.30%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -1,18 +1,38 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int c = 0;
-        for (int i = 0; i < flowerbed.length; i++) {
-            if (flowerbed[i] == 0) {
-                boolean emptyLeft = (i == 0) || (flowerbed[i - 1] == 0);
-                boolean emptyRight = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
-
-                if (emptyLeft && emptyRight) {
-                    flowerbed[i] = 1;
-                    c++;
-                    if (c >= n) return true; // Early exit
-                }
+        int fi = -1 , li = -1 ,max = 0;
+        // first and last index of 1
+        for(int i = 0 ; i < flowerbed.length ; i++){
+            if(flowerbed[i] == 0){
+                continue;
+            }else if(fi == -1){
+                fi = i;
+                li = i;
+            }else{
+                li = i;
             }
         }
-        return c >= n;
+
+        if(fi==-1){
+            return (flowerbed.length+1)/2 >= n;
+        }
+        // no of zeros before , after 1
+        max = fi / 2;
+        max += (flowerbed.length-li-1)/2;
+
+        //middle part 1.......1
+        int c = 0;
+        for(int i = fi+1 ; i<=li-1 ;i++){
+            if(flowerbed[i] == 0){
+                c++;
+            }else{
+                max += (c-1)/2;
+                c = 0;
+            }
+        }
+
+        max += (c-1)/2;
+
+        return max >= n;
     }
 }
