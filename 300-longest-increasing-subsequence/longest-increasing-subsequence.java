883ff1bf03bc8432386c54e50a46e8c7ea261949
Danish Sheikh <119788393+Dani7867@users.noreply.github.com>
2025-08-20 10:08:11 +0530

    Time: 36 ms (76.37%) | Memory: 44 MB (95.17%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,38 +1,22 @@
 class Solution {
-
     public int lengthOfLIS(int[] nums) {
-        List<Integer> sub = new ArrayList<>();
-
-        for (int num : nums) {
-            int i = Collections.binarySearch(sub, num);
-
-            if (i < 0) {
-                i = -(i + 1); // insertion point
-            }
-
-            if (i == sub.size()) {
-                sub.add(num);
-            } else {
-                sub.set(i, num); // replace to keep subsequence optimal
-            }
+        
+        if (nums == null || nums.length == 0) return 0;
+        int[] dp = new int[nums.length];
+        int maxLIS = 1;
+        for(int i = 0 ; i < nums.length ; i++){
+            dp[i] = 1;
         }
 
-        return sub.size();
+       for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLIS = Math.max(maxLIS , dp[i]);
+       
+        }
+    return maxLIS;
     }
-
-
-   
-    // public int lengthOfLIS(int[] nums) {
-    //     return helper(nums,-1,0);
-    // }
-
-    // public int helper(int[] arr ,int pi , int ci){
-    //     if(ci == arr.length) return 0;
-    //     int t= 0;
-    //     if(pi == -1 || arr[ci] > arr [pi]){
-    //          t = 1 + helper(arr,ci,ci+1);
-    //     }
-    //     int nt = helper(arr,pi,ci+1);
-    //     return Math.max(t,nt);
-    // }
 }
