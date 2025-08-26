2e96c2eccfe736d120bc0baa80e9051aa91e291f
Danish Sheikh <119788393+Dani7867@users.noreply.github.com>
2025-08-26 12:52:40 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (18.49%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int fib(int n) {
+        if(n <= 1){
+            return n;
+        }
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+
+        for(int i = 1 ; i < n ; i++){
+            if(i == 1){
+                dp[i] = dp[i-1];
+            }else{
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+        return dp[n-1];
+    }
+}
