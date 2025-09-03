5e2a12cd0a0068572cbc259efa47a1164f32d79f
Danish Sheikh <119788393+Dani7867@users.noreply.github.com>
2025-09-03 19:33:37 +0530

    Time: 4 ms (79.00%) | Memory: 44.6 MB (51.36%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -8,32 +8,26 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
-
-        if (l1.val < l2.val) {
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
-        } else {
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
-        }
-    }
-
+    // O(nlog(k)) instead of traditional way O(nlogn)
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists.length == 0) return null;
-        return divideAndConquer(lists, 0, lists.length - 1);
-    }
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
 
-    private ListNode divideAndConquer(ListNode[] lists, int left, int right) {
-        if (left == right) return lists[left];
-
-        int mid = left + (right - left) / 2;
-        ListNode l1 = divideAndConquer(lists, left, mid);
-        ListNode l2 = divideAndConquer(lists, mid + 1, right);
-        return mergeTwoLists(l1, l2);
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
+        for (ListNode node : lists) {
+            if(node != null){
+                pq.offer(node);
+            }
+        }
+        while (!pq.isEmpty()) {
+            ListNode minNode = pq.poll();
+            tail.next = minNode;
+            tail = tail.next;
+            if(minNode.next != null){
+                pq.offer(minNode.next);
+            }
+        }
+        return dummy.next;
     }
 }
