37d13f4c0707e421b9c074072e8b9b62a65a7d87
Danish Sheikh <119788393+Dani7867@users.noreply.github.com>
2025-09-18 23:24:31 +0530

    Time: 2 ms (73.80%) | Memory: 53.4 MB (30.00%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,35 +1,13 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int c = findME(nums);
-        if(isME(nums,c)){
-            return c;
-        }
-
-        return -1;
-    }
-
-    int findME(int[] nums){
-        int c = 0;
-        int a = 0;
-        for(int x : nums){
-            if(c == 0){
-                a = x;
+        int count = 0 , candidate = 0;
+        for(int n : nums){
+            if(count == 0){
+                candidate = n;
             }
 
-            c += (a==x) ? 1 : -1; 
+            count += (n == candidate) ? 1 : -1;
         }
-
-        return a;
-    }
-
-    boolean isME(int[] nums ,int a ){
-        int c = 0;
-        for(int x : nums){
-            if(a==x){
-                c++;
-            }
-        }
-
-        return c>nums.length/2 ? true : false;
+        return candidate;
     }
 }
